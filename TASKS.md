3c75d56ec563ab7a0c0b77210d5df62f2037a9c2
Moss Collum <moss.collum@gmail.com>
2011-01-29 15:18:26 -0500

    Create, edit, and delete holidays.

--- TASKS.md
+++ TASKS.md
@@ -8,10 +8,11 @@ DONE
 * UI to add, delete stories.
 * Clean up UI.
 * UI to edit stories.
+* UI to add, edit, delete holidays.
 
 WIP
 ---
 
 TODO
 ----
-* UI to add, edit, delete holidays.
+* Oh, hey, what happens if a story starts or finishes on a day marked as a holiday?
--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -1,2 +1,16 @@
 class HolidaysController < ApplicationController
+  def index
+    @holidays = Holiday.all :order => "date DESC"
+    @new_holiday = Holiday.new
+  end
+
+  def create
+    Holiday.create params[:holiday]
+    redirect_to :action => :index
+  end
+
+  def destroy
+    Holiday.find(params[:id]).delete
+    redirect_to :action => :index
+  end
 end
