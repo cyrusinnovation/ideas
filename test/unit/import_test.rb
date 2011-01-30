fcfdf47cf33f97debd07055030cbf2b444101b95
Moss Collum <moss.collum@gmail.com>
2011-01-30 17:15:45 -0500

    Apparently it already does right by nils.

--- test/unit/import_test.rb
+++ test/unit/import_test.rb
@@ -10,6 +10,15 @@ class ImportTest < ActiveSupport::TestCase
     assert_equal Date.new(2011, 1, 25), story.finished
   end
 
+  test "blank fields should become nil" do
+    story = import_story '143 Remove unused permissions,,,,,,,,'
+    assert_equal '143 Remove unused permissions', story.title
+    assert_nil story.team
+    assert_nil story.estimate
+    assert_nil story.started
+    assert_nil story.finished
+  end
+
   def import_story csv_row
     import_file = StringIO.new <<HERE # Note that it should ignore the first (header) line of the file
 Title,Team,Estimate,Started,Finished,Additional,Other,Ignored,Fields
