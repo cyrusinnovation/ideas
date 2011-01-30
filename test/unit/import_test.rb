b34e2b7f7669113d71f03b74c9812a777054de62
Moss Collum <moss.collum@gmail.com>
2011-01-30 17:19:20 -0500

    Apparently the rules for escaping in CSV files are more consistent than I imagined. All my tests pass right away!

--- test/unit/import_test.rb
+++ test/unit/import_test.rb
@@ -19,6 +19,16 @@ class ImportTest < ActiveSupport::TestCase
     assert_nil story.finished
   end
 
+  test "wrap a field in quotes if it includes commas" do
+    story = import_story '"a, b, c",,,,,,,,'
+    assert_equal 'a, b, c', story.title
+  end
+
+  test "escape quotes by doubling them" do
+    story = import_story '"a, ""b"", c",,,,,,,,'
+    assert_equal 'a, "b", c', story.title
+  end
+
   def import_story csv_row
     import_file = StringIO.new <<HERE # Note that it should ignore the first (header) line of the file
 Title,Team,Estimate,Started,Finished,Additional,Other,Ignored,Fields
