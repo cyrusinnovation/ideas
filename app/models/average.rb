3f2daa9b73f4728f225ff76dbe128852ed2dc10f
Moss Collum <moss.collum@gmail.com>
2011-02-03 11:38:37 -0500

    Average, standard deviation, making from objects to values for average, rejecting nils.

--- app/models/average.rb
+++ app/models/average.rb
@@ -1,9 +1,26 @@
 class Average
-  def initialize values
-    @values = values
+  def initialize values, &block
+    if block_given?
+      values = values.map &block
+    end
+    @values = values.reject {|v| v.nil? }
   end
 
   def mean
-    @values.reduce(:+) / @values.size
+    @values.reduce(:+) / @values.size.to_f
+  end
+
+  def standard_deviation
+    Math.sqrt variance
+  end
+
+  def variance
+    mean = self.mean
+    sum_of_squared_deviations = @values.reduce(0) {|sum, value| sum + (value - mean)**2 }
+    sum_of_squared_deviations / @values.size
+  end
+
+  def to_html
+    "%.1f &plusmn; %.1f" % [mean, standard_deviation]
   end
 end
--- test/unit/average_test.rb
+++ test/unit/average_test.rb
@@ -2,8 +2,40 @@ require 'test_helper'
 
 class AverageTest < ActiveSupport::TestCase
   test "averages numbers" do
+    a = Average.new [2, 3, 6, 7]
+
+    assert_equal 4.5, a.mean
+  end
+
+  test "calculates standard deviation" do
     a = Average.new [2, 3, 4, 7]
 
-    assert_equal 4, a.mean
+    # variances are -2, -1, 0, 3
+    # squared variances are 4, 1, 0, 9
+    # mean of those is 3.5
+    assert_equal Math.sqrt(3.5), a.standard_deviation
+    assert_equal 1, Average.new([3, 3, 5, 5]).standard_deviation, "a simpler case"
+  end
+
+  test "renderable as html" do
+    a = Average.new [3, 3, 5, 5]
+    assert_equal "4.0 &plusmn; 1.0", a.to_html
+  end
+
+  test "shows 1 decimal place in html" do
+    a = Average.new [1, 2, 7, 2, 6, 7]
+    assert_equal "4.2 &plusmn; 2.5", a.to_html
+  end
+
+  test "maps and filters nils if given a block" do
+    s2 = Story.new :estimate => 2
+    s3 = Story.new :estimate => 3
+    s6 = Story.new :estimate => 6
+    s7 = Story.new :estimate => 7
+    no_estimate = Story.new
+
+    a = Average.new([s2, s3, s6, s7, no_estimate]) {|s| s.estimate }
+
+    assert_equal 4.5, a.mean
   end
 end
