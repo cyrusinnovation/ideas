3419d6f09db316d67460e06b539c87963aa96255
Moss Collum <moss.collum@gmail.com>
2011-05-07 11:37:40 -0700

    Deleted some unused code.

--- app/helpers/cycle_time_helper.rb
+++ app/helpers/cycle_time_helper.rb
@@ -1,13 +1,2 @@
 module CycleTimeHelper
-  def full_team_link
-    attributes = {}
-    attributes[:class] = 'current' if @controller.action_name == 'index'
-    link_to 'Full Team', { :action => 'index' }, attributes
-  end
-
-  def team_link team
-    attributes = {}
-    attributes[:class] = 'current' if @controller.action_name == 'team' && @team == team
-    link_to team.name, { :action => 'team', :team => team }, attributes
-  end
 end
