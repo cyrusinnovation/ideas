3366100238f87ca6731cddd65677e1df2b34e272
Moss Collum <moss.collum@gmail.com>
2011-05-14 19:03:21 -0400

    Added UI to adjust the number of estimation examples shown.

--- app/controllers/estimation_view_controller.rb
+++ app/controllers/estimation_view_controller.rb
@@ -2,25 +2,26 @@ class EstimationViewController < ApplicationController
   TARGET_BURN_RATE = 16
 
   def index
-    averages = Story.average_hours_by_estimate
+    @count = params.fetch(:count, 3).to_i
+    @count = 1 if @count < 1
     @groups = [
-        examples(averages, 5),
-        examples(averages, 3),
-        examples(averages, 2),
-        examples(averages, 1),
-        examples(averages, 0.5),
-        examples(averages, 0.25),
+        examples(5),
+        examples(3),
+        examples(2),
+        examples(1),
+        examples(0.5),
+        examples(0.25),
     ]
   end
 
   private
 
-  def examples(averages, estimate)
+  def examples( estimate)
     target = TARGET_BURN_RATE * estimate
     EstimationStory.find_examples(:estimate => estimate,
                                   :target => target,
                                   :min => target * 0.75,
                                   :max => target * 1.25,
-                                  :count => 5)
+                                  :count => @count)
   end
 end
--- test/functional/estimation_view_controller_test.rb
+++ test/functional/estimation_view_controller_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class EstimationViewControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    stub(EstimationStory).find_examples {|options| [estimation_story] * options[:count] }
+  end
+
+  test "shows three estimation examples by default" do
+    get :index
+
+    assert_equal 3, assigns(:count)
+    assigns(:groups).each {|group| assert_equal 3, group.size }
+  end
+
+  test "shows a different number of estimation examples if requested" do
+    get :index, :count => 5
+
+    assert_equal 5, assigns(:count)
+    assigns(:groups).each {|group| assert_equal 5, group.size }
+  end
+
+  test "refuses to show less than one story" do
+    get :index, :count => -7
+
+    assert_equal 1, assigns(:count)
+    assigns(:groups).each {|group| assert_equal 1, group.size }
+  end
+
+  def estimation_story
+    EstimationStory.new 'some story', 3, 2
   end
 end
