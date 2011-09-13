3aa5725c4e1901a9370328025203e22f27d96b78
Paul Infield-Harm <paul@infield-harm.org>
2011-09-12 22:03:15 -0400

    Restore commented out import code

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -3,8 +3,8 @@ class ImportsController < ApplicationController
   end
 
   def create
-#    Story.delete_all
-#    Import.new(params[:file]).each {|story| story.save }
+    Story.delete_all
+    Import.new(params[:file]).each {|story| story.save }
     redirect_to stories_path
   end
   
