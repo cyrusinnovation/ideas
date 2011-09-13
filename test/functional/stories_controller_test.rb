eded801bdf743864a34c4fc5686913cdcc8660aa
Matt Raibert <mattraibert@gmail.com>
2011-09-13 11:31:15 -0400

    moving the imports test to the right place

--- test/functional/stories_controller_test.rb
+++ test/functional/stories_controller_test.rb
@@ -36,12 +36,4 @@ class StoriesControllerTest < ActionController::TestCase
     assert !Story.exists?(id), "should have been deleted"
     assert_redirected_to :action => "index"
   end
-
-  test "import stories from a CSV file" do
-    file = fixture_file_upload 'files/import_csv.txt', 'text/plain'
-
-    post :import, :file => file
-
-    assert_equal 5, Story.count, "Should clear stories from DB and import the 5 new ones"
-  end
 end
