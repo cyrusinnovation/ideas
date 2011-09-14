c51e6e35f984d22230b8563595f7a4185d296315
Matt Raibert <mattraibert@gmail.com>
2011-09-14 16:48:20 -0400

    one more place queries for stories through the user

--- app/controllers/burn_rate_controller.rb
+++ app/controllers/burn_rate_controller.rb
@@ -4,7 +4,7 @@ class BurnRateController < SecureController
         map { |s| s.burn_rate }.
         reverse.
         last(120)
-    @burn_rates = DataSeries.new burn_rate_history    
+    @burn_rates = DataSeries.new burn_rate_history
     @estimate_groups = EstimateGroups.new(Story.all_with_burn_rates) { |s| s.hours_worked }
     @stories = Story.all_with_burn_rates.map {|s| @estimate_groups.story_vs_estimate(s) }
   end
--- test/functional/burn_rate_controller_test.rb
+++ test/functional/burn_rate_controller_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class BurnRateControllerTest < ActionController::TestCase
   test "shows all stories where burn rate is known in reverse order finished" do
-    @current_user.stories.build(:title => "should appear", :estimate => 1, :hours_worked => 4, :finished => '2011-1-11').save
-    @current_user.stories.build(:title => "no estimate", :hours_worked => 4, :finished => '2011-1-12').save
-    @current_user.stories.build(:title => "no hours", :estimate => 1, :finished => '2011-1-13').save
-    @current_user.stories.build(:title => "should appear first", :estimate => 3, :hours_worked => 17, :finished => '2011-1-14').save
+    @current_user.stories.create(:title => "should appear", :estimate => 1, :hours_worked => 4, :finished => '2011-1-11')
+    @current_user.stories.create(:title => "no estimate", :hours_worked => 4, :finished => '2011-1-12')
+    @current_user.stories.create(:title => "no hours", :estimate => 1, :finished => '2011-1-13')
+    @current_user.stories.create(:title => "should appear first", :estimate => 3, :hours_worked => 17, :finished => '2011-1-14')
 
     get :index
 
