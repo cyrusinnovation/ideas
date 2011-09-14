622b02f5ca1e9cae56d7f7f8557c791d48fe3ffe
Paul Infield-Harm <paul@infield-harm.org>
2011-09-14 14:27:57 -0400

    Add front controller and page

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Pointilist::Application.routes.draw do
   resources :stories
   resource :import, :only => [:new, :create]
 
+  root :to => 'home#index', :constraints => lambda {|r| r.env["warden"].authenticate? }
+  root :to => 'front#index'
+
+  get "/" => 'home#index', :as => "user_root"
+
   match '/burn_rate' => 'burn_rate#index'
   match '/estimation_view' => 'estimation_view#index'
   
-  devise_for :users do
-    root :to => "home#index"
-  end
-  
-  root :to => "stories#index"
+  devise_for :users
 end
