0776acc8901d97e8f3ae9edd0299b7df5040fa15
Matt Raibert <mattraibert@gmail.com>
2011-09-16 12:00:18 -0400

    -mmoved burn rates methods from Story to User so they can be scoped by user_id

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,14 +2,6 @@ class Story < ActiveRecord::Base
   composed_of :date_range, :mapping => [%w(started started), %w(finished finished)]
   belongs_to :user
 
-  def self.all_with_burn_rates
-    all :conditions => "estimate IS NOT NULL AND hours_worked IS NOT NULL", :order => "finished DESC"
-  end
-
-  def self.hours_worked_by_estimate
-    DataSeries.by_group(all_with_burn_rates, :group => :estimate) {|s| s.hours_worked }
-  end
-
   def burn_rate
     return nil if hours_worked.nil?
     return nil if estimate.nil?
@@ -20,5 +12,15 @@ class Story < ActiveRecord::Base
     return -1 if started.nil?
     return 1 if other.started.nil?
     other.started <=> started
+  end
+end
+
+class User
+  def hours_worked_by_estimate
+    DataSeries.by_group(all_with_burn_rates, :group => :estimate) {|s| s.hours_worked }
+  end
+
+  def all_with_burn_rates
+    stories.where("estimate IS NOT NULL AND hours_worked IS NOT NULL").order("finished DESC")
   end
 end
