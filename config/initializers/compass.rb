fb4042304b0074bed86832bdffb2cb00d0adf8ca
Paul Infield-Harm <paul@infield-harm.org>
2011-09-20 17:01:46 -0400

    Initial application of bootstrap style

--- config/initializers/compass.rb
+++
@@ -1,8 +0,0 @@
-# Configuration for compass on heroku see http://devcenter.heroku.com/articles/using-compass
-
-require 'fileutils'
-FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
-
-Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
-    :urls => ['/stylesheets'],
-    :root => "#{Rails.root}/tmp")
