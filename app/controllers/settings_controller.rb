eb8b14aecdd1b2989a842603374b748820ec24b5
Paul Infield-Harm <paul@infield-harm.org>
2011-09-22 14:39:27 -0400

    Change redirect behavior for settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,11 +1,10 @@
 class SettingsController < ApplicationController
   def edit
-    session[:return_to] = request.referer
   end
 
   def update
     current_user.target_point_size = params[:user][:target_point_size]
     current_user.save
-    redirect_to session[:return_to]
+    redirect_to :action => :edit
   end
 end
