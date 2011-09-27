6f198a03fdfdbd985bf189457dac06aee6446617
Paul Infield-Harm <paul@infield-harm.org>
2011-09-26 20:54:16 -0400

    Speed up trend computation

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,11 +1,17 @@
 class TrendsController < SecureController
   def index
-    burn_rate_history = current_user.all_with_burn_rates.
+    
+    all = current_user.all_with_burn_rates
+    
+    burn_rate_history = all.
         map { |s| s.burn_rate }.
         reverse.
         last(120)
+        
     @burn_rates = DataSeries.new burn_rate_history
-    @estimate_groups = EstimateGroups.new(current_user.all_with_burn_rates) { |s| s.hours_worked }
-    @stories = current_user.all_with_burn_rates
+    
+    @estimate_groups = EstimateGroups.new(all) { |s| s.hours_worked }
+    
+    @stories = all
   end
 end
