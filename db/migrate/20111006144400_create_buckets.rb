37d497e730cd270415636000f3ef35666f2b75c0
Jacob <jodonnell@cyrusinnovation.com>
2011-10-06 17:19:47 -0400

    added to the migration to add buckets

--- db/migrate/20111006144400_create_buckets.rb
+++ db/migrate/20111006144400_create_buckets.rb
@@ -6,6 +6,10 @@ class CreateBuckets < ActiveRecord::Migration
       t.timestamps
       t.references :user
     end
+
+    User.all.each do |user|
+      user.buckets << [0.25,0.5, 1, 2, 3, 5, 8, 13].map { |bucket| Bucket.create :value => bucket }
+    end
   end
 
   def self.down
