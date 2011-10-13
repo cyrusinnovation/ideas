435df803b747ca63425987e8ff46c7ce21d6de59
Jacob and Matt <jodonnell@cyrusinnovation.com>
2011-10-13 14:39:13 -0400

    removed query string hacks, and turned them into new tabs

--- lib/current_project.rb
+++ lib/current_project.rb
@@ -1,13 +1,13 @@
 module CurrentProject
   def current_project
     if user_has_projects?
-      project_id = params[:project_id] || current_user.projects.first
+      project_id = params[:project_id]
       current_user.projects.find project_id
     end
   end
 
   def user_has_projects?
-    user_signed_in? && current_user.projects.size > 0
+    user_signed_in? && params[:project_id]
   end
 
 end
