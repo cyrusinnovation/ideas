caaf464fc5f4efdf5df6867ee9a7819b4217d866
Jacob and Matt <jodonnell@cyrusinnovation.com>
2011-10-13 15:51:47 -0400

    welcome page redirect to different pages depending on situation

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,5 +1,10 @@
 class FrontController < ApplicationController
   def index
+    if user_signed_in? && !current_user.projects.empty?
+      redirect_to project_stories_path(current_user.projects.first)
+    elsif user_signed_in?
+      redirect_to new_project_path
+    end
   end
 
   def privacy
