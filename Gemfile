3d19f067c3e4007e85e8fea88bd3990c5f40d921
Jacob <jodonnell@cyrusinnovation.com>
2011-11-01 18:04:35 -0400

    updated rails and added rspec

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source :rubygems
 
-gem "rails", "3.1.0"
-gem "pg", ">= 0.11.0"
+gem "rails", "3.1.1"
+gem "pg", "0.11.0"
 gem "haml", "3.1.2"
 gem "rdoc", "3.9.4"
 gem "jquery-rails", "1.0.13"
-gem 'gvis', '>= 2.0.0'
-gem 'devise', '>= 1.4.5'
+gem 'gvis', '>=2.0.0'
+gem 'devise', '1.4.5'
 gem 'less-rails-bootstrap', '1.3.2'
 gem 'uglifier', '1.0.3'
 
@@ -25,4 +25,5 @@ end
 
 group :development, :test do
   gem 'ruby-debug19'
+  gem "rspec-rails", "2.7.0"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,13 +40,9 @@ ActiveRecord::Schema.define(:version => 20111020200930) do
     t.integer  "project_id"
   end
 
-  create_table "teams", :force => true do |t|
-    t.string "name"
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => ""
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
