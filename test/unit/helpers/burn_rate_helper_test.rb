90fabc70e64319456998b18d9dea92704a55d5cd
Jacob <jodonnell@cyrusinnovation.com>
2011-11-15 13:34:38 -0500

    got rid of useless tests

--- test/unit/helpers/burn_rate_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class BurnRateHelperTest < ActionView::TestCase
-end
--- test/unit/story_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class StoryTest < ActiveSupport::TestCase
-  def setup
-    @user = User.create({email:"ben@franklin.com", password:"electric"})
-  end
-end
