825edfcf8d4b85cffcd262b27f90d590aa3dd95b
Jacob <jodonnell@cyrusinnovation.com>
2011-11-15 13:19:20 -0500

    cut out unnecessary parts of the privacy page

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,7 +4,6 @@ class SettingsController < SecureController
   end
 
   def update
-    change_buckets
     @project = update_project
 
     if @project.invalid?
@@ -19,31 +18,7 @@ class SettingsController < SecureController
   def update_project
     project = current_project
     project.name = params[:name]
-    project.target_point_size = params[:target_point_size]
     project.save
     project
-  end
-
-  def change_buckets
-    buckets = buckets_from_strings(clean_buckets(params[:buckets]))
-    current_project.buckets = buckets
-  end
-
-  def buckets_from_strings bucket_strings
-    bucket_strings.collect do |bucket_string|
-      bucket_string.strip!
-
-      bucket = current_project.buckets.find_by_value bucket_string
-      bucket = Bucket.create(:value => bucket_string) if bucket.nil?
-      bucket
-    end
-  end
-
-  def clean_buckets bucket_strings
-    buckets = bucket_strings.split ","
-    buckets.select do |bucket| 
-      bucket.strip!
-      bucket =~ /^[0-9.]+$/
-    end
   end
 end
