530455fff93567a576b181debce874a55cbcd685
Jacob <jodonnell@cyrusinnovation.com>
2011-11-15 12:03:17 -0500

    Change db settings for ideas

--- db/migrate/20111020200930_drop_table_stories_old.rb
+++
@@ -1,5 +0,0 @@
-class DropTableStoriesOld < ActiveRecord::Migration
-  def change
-    drop_table :stories_old
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111020200930) do
+ActiveRecord::Schema.define(:version => 20111020195758) do
 
   create_table "buckets", :force => true do |t|
     t.float    "value"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(:version => 20111020200930) do
 
   create_table "stories", :force => true do |t|
     t.string   "title"
-    t.decimal  "estimate"
+    t.decimal  "estimate",     :precision => 3, :scale => 1
     t.date     "finished"
     t.decimal  "hours_worked"
     t.datetime "created_at"
