95f2cb3f8b6678aa4fda1fdbb9b3a82bf50b36b4
Jacob O'Donnell <jacobodonnell@gmail.com>
2011-11-22 10:40:54 -0500

    trying to use params[:plain]

--- app/controllers/incoming_mail_controller.rb
+++ app/controllers/incoming_mail_controller.rb
@@ -4,18 +4,11 @@ class IncomingMailController < ApplicationController
 
   def create
     message = Mail.new(params[:message])
-    Rails.logger.add Logger::INFO, params[:message]
     user = User.find_by_email(message.from)
 
     if user
-      if mail.multipart?
-        text = plain_text_body(mail)
-      else
-        text = message.body.decoded
-      end
-      
       user.projects.each do |project|
-        project.ideas << Idea.create(:title => message.subject, :description => text)
+        project.ideas << Idea.create(:title => message.subject, :description => params[:plain])
       end
     
       render :text => 'success', :status => 200
@@ -23,23 +16,5 @@ class IncomingMailController < ApplicationController
       render :text => 'failure', :status => 404
     end
   end
-
-  def plain_text_body(email)
-    parts = email.parts.collect {|c| (c.respond_to?(:parts) && !c.parts.empty?) ? c.parts : c}.flatten
-    if parts.empty?
-      parts << email
-    end
-    plain_text_part = parts.detect {|p| p.content_type == 'text/plain'}
-    if plain_text_part.nil?
-      # no text/plain part found, assuming html-only email
-      # strip html tags and remove doctype directive
-      plain_text_body = strip_tags(email.body.to_s)
-      plain_text_body.gsub! %r{^<!DOCTYPE .*$}, ''
-    else
-      plain_text_body = plain_text_part.body.to_s
-    end
-    plain_text_body.strip
-  end
-
 end
 
