bdc450222ec768c683de4bab4cf4a62bb163f386
Jacob <jodonnell@cyrusinnovation.com>
2011-11-29 11:36:26 -0500

    changed redirect to all ideas page rather than create an idea

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,7 +1,7 @@
 class FrontController < ApplicationController
   def index
     if user_signed_in? && !current_user.projects.empty?
-      redirect_to project_ideas_path(current_user.projects.first)
+      redirect_to project_history_path(current_user.projects.first)
     elsif user_signed_in?
       redirect_to new_project_path
     end
